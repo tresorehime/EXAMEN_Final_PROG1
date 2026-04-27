d13b184fe6afca63eecf4a7d0f5189c45f1afa24
FRANÇOISE Anthonia <hei.anthonia@gmail.com>
2026-04-27 21:16:32 +0300

    fix: change function name

--- encodage et decodage/encodage.js
+++ encodage et decodage/encodage.js
@@ -1,7 +1,7 @@
 import { log } from 'console';
 import fs from 'fs';
 
-const encodage = path => {
+const encodageImageToBase64 = path => {
     const data = fs.readFileSync(path);
     const base64 = data.toString('base64');
     return base64;
