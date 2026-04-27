8e2f145329ad4a4187cac89e5fdb47af90a9ad13
FRANÇOISE Anthonia <hei.anthonia@gmail.com>
2026-04-27 21:05:32 +0300

    feat: add one bonus function encodage

---
+++ encodage et decodage/encodage.js
@@ -0,0 +1,9 @@
+import { log } from 'console';
+import fs from 'fs';
+
+const encodage = path => {
+    const data = fs.readFileSync(path);
+    const base64 = data.toString('base64');
+    return base64;
+}
+console.log(encodage("test.jpeg"));
